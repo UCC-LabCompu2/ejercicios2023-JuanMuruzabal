3f3c1916a42b5ce7ffb4ea9a7918c7a1877752cb
JuanMuruzabal <muruzabaljuan@gmail.com>
2023-06-06 22:42:03 -0300

    Se agrega canvas

--- Proyecto2023-Masholder-Muruzabal/Index_Javascript.js
+++ Proyecto2023-Masholder-Muruzabal/Index_Javascript.js
@@ -132,3 +132,56 @@ ChequeoValores = (id, valor, tipo) => {
         }
     }
 }
+
+
+//AnimacionDepreparacion
+/**
+ * Funcion que muestra una pelotita rebotando dentro del canvas
+ * @method animacion
+ */
+
+animacion = () => {
+
+    const RADIUS = 3;
+    const WIDTH = 50;
+    const HEIGHT = 50;
+    const SPEED = 1;
+
+    const coord = { x: WIDTH / 2, y: HEIGHT / 2 };
+    const delta = {
+      x: Math.random() * SPEED * 2 - SPEED,
+      y: Math.random() * SPEED * 2 - SPEED
+    };
+
+    const cnv = document.getElementById('canvas');
+    const ctx = cnv.getContext('2d');
+
+    function tick() {
+      const newx = coord.x + delta.x;
+      const newy = coord.y + delta.y;
+
+      if (newx > WIDTH - RADIUS || newx < RADIUS)
+        delta.x = -delta.x;
+
+      if (newy > HEIGHT - RADIUS || newy < RADIUS)
+        delta.y = -delta.y;
+
+      coord.x = newx;
+      coord.y = newy;
+
+      requestAnimationFrame(draw);
+      setTimeout(tick, 10);
+    }
+
+    function draw() {
+      ctx.fillStyle = "white";
+      ctx.clearRect(0, 0, WIDTH, HEIGHT);
+      ctx.beginPath();
+      ctx.arc(coord.x, coord.y, RADIUS, 0, Math.PI * 2, 1);
+      ctx.fill();
+    }
+
+    tick();
+
+
+}
